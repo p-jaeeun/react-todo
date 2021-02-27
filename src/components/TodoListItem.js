c899d5dc8c2ee9eb391ac3e47e2dfe7ad861cc6f
p-jaeeun <j_brick@naver.com>
2021-02-27 21:23:35 +0900

    컴포넌트 성능 & 렌더링 최적화

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -7,25 +7,34 @@ import {
 import cn from 'classnames';
 import './TodoListItem.scss';
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
   const { id, text, checked } = todo;
 
   return (
-    <div className="TodoListItem">
-      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
-        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-        <div className="text">{text}</div>
-      </div>
-      <div
-        className="remove"
-        onClick={() => {
-          onRemove(id);
-        }}
-      >
-        <MdRemoveCircleOutline />
+    <div className="TodoListItem-virtualized" style={style}>
+      <div className="TodoListItem">
+        <div
+          className={cn('checkbox', { checked })}
+          onClick={() => onToggle(id)}
+        >
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          <div className="text">{text}</div>
+        </div>
+        <div
+          className="remove"
+          onClick={() => {
+            onRemove(id);
+          }}
+        >
+          <MdRemoveCircleOutline />
+        </div>
       </div>
     </div>
   );
 };
-
-export default TodoListItem;
+// React.mom(): 컴포넌트의 propsr가 바뀌지 않으면, 리렌더링 하지 않도록 설정
+// TodoListItem은 todo, onRemove,onToggle이 바뀌지 않으면 , 리렌더링 안함
+export default React.memo(
+  TodoListItem,
+  (prevProps, nextProps) => prevProps.todo === nextProps.todo,
+);
